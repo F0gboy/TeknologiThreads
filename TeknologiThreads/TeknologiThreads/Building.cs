9e449550ba5fb254a5080b87981b75b99f8979c9
F0gboy <Jasp3r@live.dk>
2024-02-06 10:12:04 +0100

    klassediagram sat ind

---
+++ TeknologiThreads/TeknologiThreads/Building.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeknologiThreads
+{
+    internal class Building
+    {
+        public int currentWorkers;
+        public int maxWorkers;
+        public bool lockTaken;
+        public int TaskTime;
+
+        public Building() 
+        {
+        
+        }
+    }
+}
---
+++ TeknologiThreads/TeknologiThreads/Worker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeknologiThreads
+{
+    internal class Worker
+    {
+        public int Cost;
+        public int resourceGain;
+        public int speed;
+
+        public Worker()
+        {
+        
+        }
+    }
+}
